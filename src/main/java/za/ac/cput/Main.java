c2f3cb2996377ea36a276b21276ef915b4b7837f
Lubzie1007 <lubznotutela17@gmail.com>
2024-11-26 13:02:46 +0200

    frontend

--- src/main/java/za/ac/cput/Main.java
+++ src/main/java/za/ac/cput/Main.java
@@ -3,7 +3,7 @@ package za.ac.cput;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"za.ac.cput"})
 public class Main {
     public static void main(String[] args) {
 
